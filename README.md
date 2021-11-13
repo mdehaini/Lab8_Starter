84b6561774f9e20962ef7b87bafcb802c3c660ee
mdehaini <mdehaini@ucsd.edu>
2021-11-13 14:43:52 -0800

    lab 8 completed

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Lab 8 - Starter
+
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+We would fit them within a github action that runs whenever code is pushed. Firstly, this would make it automatic so that the developer does not have to worry and remember to run the test every time they push code (which is why 2 is wrong). We also need to test consistently, because we do not want to have an end product that is a jumble of code and mistakes. Rather, we want to continually test to assure every push of code adds to the application and works rather than adding errors and causing it to crash (which is why 3 is wrong).
+
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+   
+Yes, we are emulating user actions from start to finish and making sure our application works correctly. Recieving the correct output is part of that process
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+   
+No, I would not because messgae will be an end-to-end process that requires components to interact with each other. This would not be testing an individual part of code which is why we would not use a unit test on this feature. 
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes, getting length is an individual part of the code that would not interact with another feature which means we can use unit testing. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('(111)111-1111 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(111)111-1111')).toBe(true);
+});
+
+test('(222)222-2222 is a valid phone number', () => {
+    expect(functions.isPhoneNumber('(222)222-2222')).toBe(true);
+});
+
+test('111111111 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('111111111')).toBe(false);
+});
+
+test('(111)111-111 is not a valid phone number', () => {
+    expect(functions.isPhoneNumber('(111)111-111')).toBe(false);
+});
+
+test('unicorns@gmail.com is a valid email', () => {
+    expect(functions.isEmail('unicorns@gmail.com')).toBe(true);
+});
+
+test('unicorns@hotmail.com is a valid email', () => {
+    expect(functions.isEmail('unicorns@hotmail.com')).toBe(true);
+});
+
+test('unicorns is a valid email', () => {
+    expect(functions.isEmail('unicorns')).toBe(false);
+});
+
+test('unicorns@gmail is a valid email', () => {
+    expect(functions.isEmail('unicorns@gmail ')).toBe(false);
+});
+
+test('aBrW2 is a valid strong password', () => {
+    expect(functions.isStrongPassword('aBrW2')).toBe(true);
+});
+
+test('ahduhdafuisdhf is a valid strong password', () => {
+    expect(functions.isStrongPassword('ahduhdafuisdhf')).toBe(true);
+});
+
+test('adhosfosfjdskvniosdfiodsVNoisdfjods is not a valid strong password', () => {
+    expect(functions.isStrongPassword('adhosfosfjdskvniosdfiodsVNoisdfjods')).toBe(false);
+});
+
+test('dj is not a valid strong password', () => {
+    expect(functions.isStrongPassword('dj')).toBe(false);
+});
+
+test('06 / 05 / 2022 is a valid date', () => {
+    expect(functions.isDate('06/05/2022')).toBe(true);
+});
+
+test('05 / 01 / 2002 is a valid date', () => {
+    expect(functions.isDate('05/01/2002')).toBe(true);
+});
+
+test('06 - 05 - 2022 is not a valid date', () => {
+    expect(functions.isDate('06-5-2022')).toBe(false);
+});
+
+test('05 / 01 is not a valid date', () => {
+    expect(functions.isDate('05/01')).toBe(false);
+});
+
+test('903825 is a valid hex color code', () => {
+    expect(functions.isHexColor('903825')).toBe(true);
+});
+
+test('903825 is a valid hex color code', () => {
+    expect(functions.isHexColor('4A1D14')).toBe(true);
+});
+
+test('5 is not a valid hex color code', () => {
+    expect(functions.isHexColor('5')).toBe(false);
+});
+
+test('903825 is not a valid hex color code', () => {
+    expect(functions.isHexColor('9038255')).toBe(false);
+});
